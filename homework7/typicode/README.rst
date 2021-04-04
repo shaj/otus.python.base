4c76f4d3936227b6a67d75a5a0d270f804fe7c02
ShajTester <kolobrod2002@yandex.ru>
2021-04-04 21:26:50 +0300

    HW7: first migration

--- homework7/typicode/README.rst
+++ homework7/typicode/README.rst
@@ -3,3 +3,37 @@
 
 *За основу взята база из homework3*
 
+TODO
+-----
+
+1. Перенести модель в Djando
+2. Подключиться к postgres. Проверить psycopg2.
+3. сделать миграции
+4. Очистка базы и заполнение тестовыми данными
+   - применимо для Балука
+5. Перенести статику и разметку
+6. 
+
+
+Темы уроков
+-----------
+
+**Урок 26** - Знакомство с Django. админка; модели; миграции.
+
+**Урок 27** - Django ORM, django-debug-toolbar. 
+
+  - связи OneToOne, OneToMany, ManyToMany;
+  - select_related/prefetch_related;
+  - django-debug-toolbar.
+
+**Урок 28** - Django и отложенные задачи.
+
+  - celery и rabbitmq;
+  - отправка электронной почты в неблокирующем режиме;
+  - проверка статуса отложенной задачи.
+    
+**Урок 29** - больше Django.
+
+  - Class Based views;
+  - generics;
+  - Django Forms.
--- homework7/typicode/typicode/posts/models.py
+++ homework7/typicode/typicode/posts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+
+    fullname = models.CharField(max_length=32)
+    username = models.CharField(max_length=32, unique=True)
+    phone = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    website = models.CharField(max_length=128)
+
+    street = models.CharField(max_length=32)
+    suite = models.CharField(max_length=32)
+    city = models.CharField(max_length=32)
+    zipcode = models.CharField(max_length=32)
+    geo_lat = models.CharField(max_length=32)
+    geo_lng = models.CharField(max_length=32)
+
+    company_name = models.CharField(max_length=32)
+    company_catchPhrase = models.CharField(max_length=128)
+    company_bs = models.CharField(max_length=128)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r}, is_staff={self.is_staff})"
+
+    def __repr__(self):
+        return str(self)
