da2017959c5ec817889e0e185f72a387b9f8993d
ShajTester <kolobrod2002@yandex.ru>
2021-04-06 18:03:09 +0300

    HW-7: WIP

--- homework7/typicode/typicode/posts/urls.py
+++ homework7/typicode/typicode/posts/urls.py
@@ -13,12 +13,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-import debug_toolbar
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('__debug__/', include(debug_toolbar.urls)),
-    path('', include('posts.urls')),
+    path('', views.main_view),
+    path('post/<int>', views.post_detail),
+    path('newpost/', views.new_post),
 ]
